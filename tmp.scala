15a769a2a2750da778be4194ef77ad93db4eb32b
Kevan Hollbach <khollbach@gmail.com>
2019-07-23 23:27:58 -0400

    Working through lectures still

--- tmp.scala
+++ tmp.scala
@@ -1,17 +1,23 @@
 object Main {
   def main(args: Array[String]): Unit = {
-    println(nested(5))
+    println()
   }
 
-  def isPrime(n: Int): Boolean =
-    n >= 2 && (2 to n - 1 forall (x => n % x != 0))
+  def queens(n: Int): Set[List[Int]] = {
+    def placeQueens(k: Int): Set[List[Int]] = {
+      if (k == 0) Set(List())
+      else
+        for {
+          queens <- placeQueens(k - 1)
+          col <- 0 until n
+          if isSafe(col, queens)
+        } yield col :: queens
+    }
 
-  def nested(n: Int): List[(Int, Int)] = {
-    /** Find all $1 \le j < i < n$ s.t. i + j is prime. */
-    val pairs =
-      1 until n flatMap (i =>
-        1 until i map (j => (i, j)))
+    placeQueens(n)
+  }
 
-    (pairs filter { case (x, y) => isPrime(x + y) }).toList
+  def isSafe(col: Int, queens: List[Int]): Boolean = {
+    ???
   }
 }
